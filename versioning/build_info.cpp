64b4c95df5e06b1bfcc4871f1d2d839b769b901d
tschumann <schumann.tom@gmail.com>
2025-01-18 17:01:30 +1000

    Update build info.

--- versioning/build_info.cpp
+++ versioning/build_info.cpp
@@ -3,10 +3,10 @@
 // generated with versioning/generate.py, AMBuild handles this
 #include <build_version_auto.h>
 
-const char* build_info::authors = "Cheeseh, RoboCop, nosoop, caxanga334";
-const char* build_info::url = "http://rcbot.bots-united.com/";
+const char* build_info::authors = "tschumann";
+const char* build_info::url = "https://www.teamsandpit.com/";
 
-const char* build_info::long_version = "APGRoboCop/rcbot2@" GIT_COMMIT_HASH;
-const char* build_info::short_version = "1.7-beta4 (apg-nosoop-caxanga334)-" GIT_COMMIT_SHORT_HASH;
+const char* build_info::long_version = "alpha-" GIT_COMMIT_HASH;
+const char* build_info::short_version = "alpha-" GIT_COMMIT_SHORT_HASH;
 
 const char* build_info::date = __DATE__;
