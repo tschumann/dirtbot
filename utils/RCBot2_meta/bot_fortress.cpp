60d4908209738548bb07a8501a9e7d8b646fbaf3
RussiaTails <113246593+RussiaTails@users.noreply.github.com>
2025-02-16 22:53:51 +0300

    Update bot_fortress.cpp
    Bots ignore anyone who is under 64th condition (a.k.a "	TF_COND_STEALTHED_USER_BUFF")

--- utils/RCBot2_meta/bot_fortress.cpp
+++ utils/RCBot2_meta/bot_fortress.cpp
@@ -7369,19 +7369,17 @@ bool CBotTF2 :: isEnemy ( edict_t *pEdict,bool bCheckWeapons )
 	{
 		if ( CBotGlobals::getTeam(pEdict) != getTeam() )
 		{
-			const int edictIndex = engine->IndexOfEdict(pEdict);
-			//TODO: To prevent bots from shooting at ghost players - like in plr_hightower_event Hell Zone [APG]RoboCop[CL] -> RE: I don't know why this code was disabled when it works fine with bots. Bots ignore anyone who has 77th and 51th tf2 conditions - RussiaTails
-			if (CTF2Conditions::TF2_IsPlayerInCondition(edictIndex, TFCond_UberchargedHidden))
-				return false; // Don't attack MvM bots who are inside spawn.
-						
-			if (CTF2Conditions::TF2_IsPlayerInCondition(edictIndex, TFCond_HalloweenGhostMode))
-				return false; // Don't attack Ghost Players
-
-			if (pEdict != nullptr && CBotGlobals::entityIsValid(pEdict)) {
-				if (pEdict != nullptr && CBotGlobals::entityIsValid(pEdict)) {
-					const int edictIndex = engine->IndexOfEdict(pEdict);
-				}
-			}
+			// TODO: To prevent bots from shooting at ghost players - like in plr_hightower_event Hell Zone [APG]RoboCop[CL]
+			// PS: It appears to crash on Windows Listenservers
+			/*if (pEdict != nullptr && CBotGlobals::entityIsValid(pEdict)) {
+				const int edictIndex = engine->IndexOfEdict(pEdict);
+				if (CTF2Conditions::TF2_IsPlayerInCondition(edictIndex, TFCond_UberchargedHidden))
+					return false; // Don't attack MvM bots who are inside spawn.
+				if (CTF2Conditions::TF2_IsPlayerInCondition(edictIndex, TFCond_HalloweenGhostMode))
+					return false; // Don't attack Ghost Players
+     				if (CTF2Conditions::TF2_IsPlayerInCondition(engine->IndexOfEdict(pEdict), TFCond_Stealthed))
+				return false; // Don't attack invisible players or bots who used magic spell - RussiaTails
+			}*/
 			
 			if ( m_iClass == TF_CLASS_SPY )	
 			{
