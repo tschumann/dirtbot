0379945462c4e0402a93eebe74aee0c5d9e85aed
tschumann <schumann.tom@gmail.com>
2025-03-29 17:15:17 +1000

    Fixed missing variable from cherry-pick.

--- utils/RCBot2_meta/bot_fortress.cpp
+++ utils/RCBot2_meta/bot_fortress.cpp
@@ -7369,17 +7369,18 @@ bool CBotTF2 :: isEnemy ( edict_t *pEdict,bool bCheckWeapons )
 	{
 		if ( CBotGlobals::getTeam(pEdict) != getTeam() )
 		{
+			const int edictIndex = engine->IndexOfEdict(pEdict);
 			//TODO: To prevent bots from shooting at ghost players - like in plr_hightower_event Hell Zone [APG]RoboCop[CL] -> RE: I don't know why this code was disabled when it works fine with bots. Bots ignore anyone who has 77th and 51th tf2 conditions - RussiaTails
 			if (CTF2Conditions::TF2_IsPlayerInCondition(edictIndex, TFCond_UberchargedHidden))
-			return false; // Don't attack MvM bots who are inside spawn.
+				return false; // Don't attack MvM bots who are inside spawn.
 						
 			if (CTF2Conditions::TF2_IsPlayerInCondition(edictIndex, TFCond_HalloweenGhostMode))
-			return false; // Don't attack Ghost Players
-			if (pEdict != nullptr && CBotGlobals::entityIsValid(pEdict)) {
+				return false; // Don't attack Ghost Players
 
+			if (pEdict != nullptr && CBotGlobals::entityIsValid(pEdict)) {
 				if (pEdict != nullptr && CBotGlobals::entityIsValid(pEdict)) {
 					const int edictIndex = engine->IndexOfEdict(pEdict);
-					}
+				}
 			}
 			
 			if ( m_iClass == TF_CLASS_SPY )	
