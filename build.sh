2005bd6a3fbb4c881e8b28e20d48ec3608b949eb
tschumann <schumann.tom@gmail.com>
2025-05-24 16:08:16 +1000

    Fixed Linux build script not using 64-bit Half-Life 2 Deathmatch binaries.

--- build.sh
+++ build.sh
@@ -34,16 +34,16 @@ echo "------------------------------------------"
 python3 ../configure.py -s hl2dm --mms-path $wd/alliedmodders/metamod-source/ --sm-path $wd/alliedmodders/sourcemod/ --hl2sdk-root $wd/alliedmodders/ --target-arch x86
 ambuild
 
-cp "rcbot.2.hl2dm/rcbot.2.hl2dm.so" "../release/dirtbot/Half-Life 2 Deathmatch/hl2mp/addons/rcbot2/bin"
-cp "loader/RCBot2Meta_i486/RCBot2Meta_i486.so" "../release/dirtbot/Half-Life 2 Deathmatch/hl2mp/addons/rcbot2/bin"
-cp -r "../package/config/" "../release/dirtbot/Half-Life 2 Deathmatch/hl2mp/addons/rcbot2/"
-
 echo "------------------------------------------"
 echo "Building for Half-Life 2: Deathmatch (x64)"
 echo "------------------------------------------"
 
 python3 ../configure.py -s hl2dm --mms-path $wd/alliedmodders/metamod-source/ --sm-path $wd/alliedmodders/sourcemod/ --hl2sdk-root $wd/alliedmodders/ --target-arch x64
 ambuild
+
+cp "rcbot.2.hl2dm.x64/rcbot.2.hl2dm.so" "../release/dirtbot/Half-Life 2 Deathmatch/hl2mp/addons/rcbot2/bin"
+cp "loader/RCBot2Meta.x64.x64/RCBot2Meta.x64.so" "../release/dirtbot/Half-Life 2 Deathmatch/hl2mp/addons/rcbot2/bin"
+cp -r "../package/config/" "../release/dirtbot/Half-Life 2 Deathmatch/hl2mp/addons/rcbot2/"
 
 echo "-------------------------------------------"
 echo "Building for Half-Life 2: Episode Two (x86)"
